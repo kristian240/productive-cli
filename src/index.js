5fde7b1d6e35cbb86cc25a0eb8050e99dfc1f262
kristian240 <kristian.djakovic@infinum.hr>
2020-08-28 23:38:15 +0200

    Add task UI and command argument support

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ const TimeEntry = require('./time-entry');
 const Config = require('./config');
 const Reports = require('./reports');
 const Logger = require('./logger');
+const Api = require('./api');
 
 const CONFIG_PATH = `${homedir}/.productivecli`;
 
@@ -16,7 +17,7 @@ const CONFIG_PATH = `${homedir}/.productivecli`;
 
   const config = await Config.getConfig(CONFIG_PATH);
   if (!config) {
-    let text = 'This is your first run, let\'s first configure it!\n';
+    let text = "This is your first run, let's first configure it!\n";
     text += 'You can find your token here:\nhttps://app.productive.io/1-infinum/settings/security';
     Logger.BoxPrint(text);
 
@@ -45,6 +46,7 @@ const CONFIG_PATH = `${homedir}/.productivecli`;
         await TimeEntry.createTimeEntry(
           argv.time,
           argv.note || '',
+          argv.task || undefined,
           today,
           config.userId,
           serviceId,
@@ -54,28 +56,96 @@ const CONFIG_PATH = `${homedir}/.productivecli`;
       }
 
       // user told us only the service
-      const { pick } = typeof argv.service !== 'undefined'
-        ? { pick: argv.service === 'food' ? 'food' : config.services[argv.service].serviceId }
-        : await inquirer.prompt([
+      const { pick } =
+        typeof argv.service !== 'undefined'
+          ? { pick: argv.service === 'food' ? 'food' : config.services[argv.service].serviceId }
+          : await inquirer.prompt([
+              {
+                type: 'list',
+                message: 'Pick an option',
+                name: 'pick',
+                choices: [
+                  ...config.services.map((s) => ({
+                    value: s.serviceId,
+                    name: `Clock on: ${s.dealName} - ${s.serviceName}`,
+                  })),
+                  {
+                    value: 'food',
+                    name: 'Clock 30mins at food',
+                  },
+                ],
+              },
+            ]);
+
+      if (pick === 'food') {
+        await TimeEntry.clockFood(headers, config, argv.date || today);
+        return;
+      }
+
+      let task = argv.task;
+
+      if (!task) {
+        const { withTask } = await inquirer.prompt([
           {
             type: 'list',
-            message: 'Pick an option',
-            name: 'pick',
+            message: 'Clock on task',
+            name: 'withTask',
             choices: [
-              ...config.services.map((s) => ({
-                value: s.serviceId,
-                name: `Clock on: ${s.dealName} - ${s.serviceName}`,
-              })),
               {
-                value: 'food', name: 'Clock 30mins at food',
+                value: true,
+                name: 'Yes',
+              },
+              {
+                value: false,
+                name: 'No',
               },
             ],
           },
         ]);
 
-      if (pick === 'food') {
-        await TimeEntry.clockFood(headers, config, argv.date || today);
-        return;
+        if (withTask) {
+          let projectId = (
+            config.services[argv.service] ||
+            config.services.find((service) => service.serviceId === pick)
+          ).projectId;
+
+          // if the service is added before task feature there is no projectId in services array
+          if (!projectId) {
+            const { included } = await Api.get('services/536162?include=deal.project', headers);
+
+            const { id } = Api.findInInluded(included, 'projects');
+
+            projectId = id;
+
+            // TODO: write it to config file for nex time
+          }
+
+          const { taskName } = await inquirer.prompt([
+            {
+              type: 'input',
+              message: 'Search for a task for by name',
+              name: 'taskName',
+            },
+          ]);
+
+          const tasks = await Api.get(
+            `tasks?filter[project_id][eq]=${projectId}&filter[title][contains]=${taskName}&sort=title`,
+            headers
+          ).then(({ data: tasks }) =>
+            tasks.map((task) => ({ value: task.id, name: task.attributes.title }))
+          );
+
+          const { taskId } = await inquirer.prompt([
+            {
+              type: 'list',
+              message: 'Pick a task from the list',
+              name: 'taskId',
+              choices: tasks,
+            },
+          ]);
+
+          task = taskId;
+        }
       }
 
       const { time = argv.time, note = argv.note } = await inquirer.prompt(
@@ -86,12 +156,22 @@ const CONFIG_PATH = `${homedir}/.productivecli`;
             name: 'time',
           },
           !argv.note && {
-            type: 'input', message: 'Note', name: 'note',
+            type: 'input',
+            message: 'Note',
+            name: 'note',
           },
         ].filter(Boolean)
       );
 
-      await TimeEntry.createTimeEntry(time, note, argv.date || today, config.userId, pick, headers);
+      await TimeEntry.createTimeEntry(
+        time,
+        note,
+        task,
+        argv.date || today,
+        config.userId,
+        pick,
+        headers
+      );
     })
     .command('timer', 'Start a timer', async () => {
       const timer = await Timer.getRunningTimer(headers, config.userId, today);
@@ -124,7 +204,9 @@ const CONFIG_PATH = `${homedir}/.productivecli`;
           ],
         },
         {
-          type: 'input', message: 'Note', name: 'note',
+          type: 'input',
+          message: 'Note',
+          name: 'note',
         },
       ]);
 
@@ -136,9 +218,13 @@ const CONFIG_PATH = `${homedir}/.productivecli`;
     .command('stats', 'Show stats', async ({ argv }) => {
       await Reports.showStats(headers, config.userId, argv.date || today);
     })
-    .command('overtime', 'Show overtime for this month (does not include today)', async ({ argv }) => {
-      await Reports.showOvertime(headers, config.userId, argv.date || today);
-    })
+    .command(
+      'overtime',
+      'Show overtime for this month (does not include today)',
+      async ({ argv }) => {
+        await Reports.showOvertime(headers, config.userId, argv.date || today);
+      }
+    )
     .demandCommand(1)
     .alias('s', 'service')
     .describe('s', 'Service')
